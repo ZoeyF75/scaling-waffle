c90eae90d00c8e3686255f49444e2d8221ed7b7a
ZoeyF75 <zf.zoodle@gmail.com>
2021-08-08 19:12:09 -0700

    getters and setters

--- employee.cpp
+++ employee.cpp
@@ -3,11 +3,35 @@
 using namespace std;
 
 class Employee {
-  
-  public:
+  private: //not required as its priv by default
   string Name; //attributes
   string Company;
   int Age;
+
+  public:
+  void setName(string name) {
+    Name = name;
+  }
+
+  string getName() {
+    return Name;
+  }
+
+  void setCompany(string company) {
+    Company = company;
+  }
+
+  string getCompany() {
+    return Company;
+  }
+
+  void setAge(int age) {
+    Age = age;
+  }
+
+  int getAge() {
+    return Age;
+  }
 
   void IntroduceYourself() {
     cout<<"Name - " << Name << endl;
